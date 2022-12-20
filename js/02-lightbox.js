5cac7c55d6f94a520be8f5bff59c0d7cce8c562f
liudmyla-ch <110854340+liudmyla-ch@users.noreply.github.com>
2022-12-20 23:29:04 +0100

    sum

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+const imagesMarkup = createImageMarkup(galleryItems);
+list.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createImageMarkup(images) {
+  return images.map(({ description, original, preview }) => {
+    return `<li class="gallery__item"><a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a></li>`;
+  })
+  .join('');
+}
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay:250});
